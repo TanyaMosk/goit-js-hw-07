9b28f31cb6855f77a6cef2a8de80f217450ed2a1
TanyaMosk <tanya856227@gmail.com>
2023-06-06 18:33:16 +0200

    1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+  `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalImage = evt.target.dataset.source;
+  const descriptionItem = evt.target.alt;
+
+  const instance = basicLightbox.create(
+    `<img src="${originalImage}"  alt = "${descriptionItem}" width="800" height="600">    
+`
+  );
+  instance.show();
+
+  const text = document.querySelectorAll("img");
+  text.forEach((el) => {
+    el.style.color = "white";
+    el.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  });
+
+  document.addEventListener("keydown", closeModalWithEscape);
+
+  function closeModalWithEscape(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" width="800" height="600" />
+  </a>
+</li> `
+);
+
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+gallery.addEventListener('click', onClick);
+function onClick(evt) {
+  evt.preventDefault();
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250", 
+});
+
 console.log(galleryItems);
